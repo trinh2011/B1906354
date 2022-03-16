c66e2802664c2817dbea49ccdbdd2c9c7dcee5da
trinh2011 <101682935+trinh2011@users.noreply.github.com>
2022-03-16 09:47:25 +0700

    Add files via upload

---
+++ form_suamk.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="vn">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SỬA MẬT KHẨU</title>
+</head>
+<body>
+   <form action="sua_mk.php" method="POST">
+        Nhập mật khẩu cũ: <input type="password" name="old_pass"><br>
+        Nhập mật khẩu mới: <input type="password" name="new_pass"><br>
+        Nhập lại mật khẩu mới: <input type="password" name="re_pass"><br>
+        <button>submit</button>
+   </form>
+</body>
+</html>
